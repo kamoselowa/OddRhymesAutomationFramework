7cf84149cf0e0cc633a03c7eb4d8c56d6b503073
Kamogelo <kamoselowa@gmail.com>
2026-04-25 18:54:36 +0200

    Setup and Teardown

--- selenium-framework/src/main/java/com/automation/base/BasePase.java
+++ selenium-framework/src/main/java/com/automation/base/BasePase.java
@@ -1,5 +1,28 @@
 package com.automation.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BasePase {
 
+    protected WebDriver driver;
+
+    public WebDriver getDriver(String browser){
+        if (browser.equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+        }
+        else if(browser.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+        }
+        else{
+            driver = new EdgeDriver();
+        }
+        return driver;
+    }
+        public void quitDriver(){
+            driver.quit();
+        }
+         
 }
