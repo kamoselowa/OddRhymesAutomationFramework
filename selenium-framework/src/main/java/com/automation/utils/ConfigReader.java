6a7dedfabb44c5c8e5a40d7aec6c2ec853b69323
Kamogelo <kamoselowa@gmail.com>
2026-04-26 18:57:38 +0200

    data Config files

---
+++ selenium-framework/src/main/java/com/automation/utils/ConfigReader.java
@@ -0,0 +1,34 @@
+package com.automation.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.bouncycastle.oer.its.etsi102941.Url;
+
+public class ConfigReader {
+
+    private Properties prop;
+
+    public ConfigReader(){
+        Properties prop = new Properties();
+        try{
+            FileInputStream fis = new FileInputStream("C:\\Users\\ratsatsi\\Documents\\OddRhymes-Automation-Framework\\selenium-framework\\src\\test\\java\\com\\automation\\Resources\\Configurations\\config.properties");
+            prop.load(fis);
+
+        }catch(IOException io){
+            io.printStackTrace();
+        }
+
+    }
+    public String getUrl(){
+        return prop.getProperty("URL");
+    }
+    public  String getBrowser(){
+        return prop.getProperty("BROWSER");
+
+    }
+    public String getGlobalWait(){
+        Long.parseLong(prop.getProperty("GLOBALWAIT"));
+    }
+}   
