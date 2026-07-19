15e5a722d687db6b6279c80578a74bf7d997c274
Kamogelo <kamoselowa@gmail.com>
2026-07-19 19:15:17 +0200

    New Tests on Register Page and added a new data reader file

--- selenium-framework/src/test/java/com/automation/tests/registration/ExistingEmail.java
+++ selenium-framework/src/test/java/com/automation/tests/registration/ExistingEmail.java
@@ -5,12 +5,18 @@ import org.testng.annotations.BeforeMethod;
 
 import com.automation.basetest.BaseTest;
 import com.fasterxml.jackson.databind.JsonSerializable.Base;
+import org.testng.annotations.Test;
 
 public class ExistingEmail extends BaseTest{
 
     @BeforeMethod
     public void SetupExistingEmail(){
         BaseSetup();
+    }
+
+    @Test
+    public void EmptyEmailFields(){
+
     }
 
     @AfterMethod
--- selenium-framework/src/test/java/com/automation/tests/registration/InvalidEmailFormat.java
+++ selenium-framework/src/test/java/com/automation/tests/registration/InvalidEmailFormat.java
@@ -2,14 +2,21 @@
 
 import com.automation.base.BasePage;
 import com.automation.basetest.BaseTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class InvalidEmailFormat extends BaseTest{
-    
+ public class InvalidEmailFormat extends BaseTest{
+    @BeforeMethod
     public void InvalidFormatSetup(){
         BaseSetup();
 
-    }
 
+
+    }
+    @Test
+
+    @AfterMethod
     public void TearDownInvalidAcc(){
         closeBrowser();
     }
