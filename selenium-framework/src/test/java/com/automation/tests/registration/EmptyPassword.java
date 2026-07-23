4a122c8e4847febf553712ff6f17a4358e9a6836
Kamogelo <kamoselowa@gmail.com>
2026-07-23 21:44:10 +0200

    Empty password implementation

--- selenium-framework/src/test/java/com/automation/tests/registration/EmptyPassword.java
+++ selenium-framework/src/test/java/com/automation/tests/registration/EmptyPassword.java
@@ -4,6 +4,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.automation.basetest.BaseTest;
+import org.testng.annotations.Test;
 
 public class EmptyPassword extends BaseTest{
 
@@ -12,6 +13,7 @@ public class EmptyPassword extends BaseTest{
         BaseSetup();
 
     }
+    @Test
     @AfterMethod
     public void TearDownEmptypassWord(){
         closeBrowser();
