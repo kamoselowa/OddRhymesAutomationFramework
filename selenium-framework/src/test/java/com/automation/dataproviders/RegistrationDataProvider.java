648ef222721461b33ec1ed59b9360fb0b5c92228
Kamogelo <kamoselowa@gmail.com>
2026-07-26 16:11:14 +0200

    status works

--- selenium-framework/src/test/java/com/automation/dataproviders/RegistrationDataProvider.java
+++ selenium-framework/src/test/java/com/automation/dataproviders/RegistrationDataProvider.java
@@ -5,15 +5,15 @@ import org.testng.annotations.DataProvider;
 
 import java.io.IOException;
 
-public class InvalidLoginTestDataProvider {
+public class RegistrationDataProvider {
 
     @DataProvider(name = "registrationData")
-    public Object[][] loginData() throws IOException {
+    public Object[][] registrationData() throws IOException {
 
         ExcelReader excelReader = new ExcelReader(
                 System.getProperty("user.dir")
                         + "/src/test/java/com/automation/Resources/Configurations/Test-Data/Test_Data_Fields_Verification.xlsx");
 
-        return excelReader.getLoginData("Login");
+        return excelReader.getRegistrationData("Registration");
     }
 }
