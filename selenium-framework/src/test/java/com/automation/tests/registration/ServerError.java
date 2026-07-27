17f64c0e5f0f02d6596cc0a1bc88feff94f54c0c
Kamogelo <kamoselowa@gmail.com>
2026-07-27 11:45:33 +0200

    new Registration sheet has been added to check if a weak password can execute with creating a new account methods are to be implemented

--- selenium-framework/src/test/java/com/automation/tests/registration/ServerError.java
+++ selenium-framework/src/test/java/com/automation/tests/registration/ServerError.java
@@ -4,13 +4,7 @@ import com.automation.basetest.BaseTest;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class ServerError extends BaseTest {
-@BeforeMethod
-    public void ServerErrorSetup(){
-    BaseSetup();
-}
-@AfterMethod
-    public void TearDownServer(){
-    closeBrowser();
-}
-}
+/*public class ServerError extends BaseTest {
+
+
+}*/
