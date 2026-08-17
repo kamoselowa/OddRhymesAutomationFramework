a9fababbb9d4fecd099eaff482178083ef6e8ea3
Kamogelo <kamoselowa@gmail.com>
2026-08-17 22:42:37 +0200

    new pages added

---
+++ selenium-framework/src/main/java/com/automation/pages/ProfilePage.java
@@ -0,0 +1,18 @@
+package com.automation.pages;
+
+import com.automation.base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class ProfilePage extends BasePage {
+
+    public ProfilePage(WebDriver driver ){
+        super(driver);
+    }
+    //========Page Elements========
+
+    @FindBy(how = How.XPATH, using = "");
+    private WebElement
+}
